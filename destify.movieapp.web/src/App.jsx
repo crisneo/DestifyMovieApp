f58ba6cebb6e302926041656237377d2f6fa4021
cristian-sanabria-idocket <108299141+cristian-sanabria-idocket@users.noreply.github.com>
2024-01-28 22:42:28 -0400

    format document

--- destify.movieapp.web/src/App.jsx
+++ destify.movieapp.web/src/App.jsx
@@ -1,20 +1,19 @@
-import * as React from 'react';
-import PropTypes from 'prop-types';
-import Tabs from '@mui/material/Tabs';
-import Tab from '@mui/material/Tab';
-import Typography from '@mui/material/Typography';
-import Box from '@mui/material/Box';
-import './App.css';
+import * as React from "react";
+import PropTypes from "prop-types";
+import Tabs from "@mui/material/Tabs";
+import Tab from "@mui/material/Tab";
+import Typography from "@mui/material/Typography";
+import Box from "@mui/material/Box";
+import "./App.css";
 
-import { MoviesTab } from './components/movies-tab';
-import { ActorsTab } from './components/actors-tab';
+import { MoviesTab } from "./components/movies-tab";
+import { ActorsTab } from "./components/actors-tab";
 
 const containerStyle = {
-    width:'100%',
-    height:'100%',
-    minHeight:'500px'
+  width: "100%",
+  height: "100%",
+  minHeight: "500px",
 };
-
 
 const CustomTabPanel = (props) => {
   const { children, value, index, ...other } = props;
@@ -28,13 +27,13 @@ const CustomTabPanel = (props) => {
       {...other}
     >
       {value === index && (
-        <Box sx={{ p: 3 }} style={{width:'800px'}}>
+        <Box sx={{ p: 3 }} style={{ width: "800px" }}>
           <Typography>{children}</Typography>
         </Box>
       )}
     </div>
   );
-}
+};
 
 CustomTabPanel.propTypes = {
   children: PropTypes.node,
@@ -45,40 +44,41 @@ CustomTabPanel.propTypes = {
 function a11yProps(index) {
   return {
     id: `simple-tab-${index}`,
-    'aria-controls': `simple-tabpanel-${index}`,
+    "aria-controls": `simple-tabpanel-${index}`,
   };
 }
 
-
 const App = () => {
-    const [value, setValue] = React.useState(0);
-    
+  const [value, setValue] = React.useState(0);
 
   const handleChange = (event, newValue) => {
     setValue(newValue);
   };
 
-    return (
-        <>
-                <h1>Movie App </h1>
-                <hr></hr>
-                <Box style={containerStyle}>
-            <Box sx={{ borderBottom: 1, borderColor: 'divider' }}>
-                <Tabs value={value} onChange={handleChange} aria-label="basic tabs example">
-                <Tab label="Movies" {...a11yProps(0)}  />
-                <Tab label="Actors" {...a11yProps(1)} />
-                </Tabs>
-            </Box>
-            <CustomTabPanel value={value} index={0}>
-                <MoviesTab />
-            </CustomTabPanel>
-            <CustomTabPanel value={value} index={1}>
-                <ActorsTab />
-            </CustomTabPanel>
-            
-            </Box>
-        </>
-    );
-}
+  return (
+    <>
+      <h1>Movie App </h1>
+      <hr></hr>
+      <Box style={containerStyle}>
+        <Box sx={{ borderBottom: 1, borderColor: "divider" }}>
+          <Tabs
+            value={value}
+            onChange={handleChange}
+            aria-label="basic tabs example"
+          >
+            <Tab label="Movies" {...a11yProps(0)} />
+            <Tab label="Actors" {...a11yProps(1)} />
+          </Tabs>
+        </Box>
+        <CustomTabPanel value={value} index={0}>
+          <MoviesTab />
+        </CustomTabPanel>
+        <CustomTabPanel value={value} index={1}>
+          <ActorsTab />
+        </CustomTabPanel>
+      </Box>
+    </>
+  );
+};
 
 export default App;
